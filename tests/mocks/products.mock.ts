b0faece8dae87d24763b9f54525ca371c52bd004
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-16 18:08:49 -0300

    Create mock allProductsFromDB

--- tests/mocks/products.mock.ts
+++ tests/mocks/products.mock.ts
@@ -17,8 +17,24 @@ const validProductFromService = {
   price: "30 peças de ouro",
 }
 
+const allProductsFromDB = [
+  {
+    id: 1,
+    name: "Excalibur",
+    price: "10 peças de ouro",
+    orderId: 1
+  },
+  {
+    id: 2,
+    name: "Espada Justiceira",
+    price: "20 peças de ouro",
+    orderId: 1
+  },
+]
+
 export default {
   productBodyCreatePost,
   validProductFromDB,
   validProductFromService,
+  allProductsFromDB,
 }
