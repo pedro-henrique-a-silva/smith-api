9f1d7aac58ef54cea1cac0b6471e161321cfc07f
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-16 16:37:08 -0300

    Integration test POST /product

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,22 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
+
+import productMock from '../../mocks/products.mock';
 
 chai.use(chaiHttp);
 
 describe('POST /products', function () { 
   beforeEach(function () { sinon.restore(); });
 
+  it('should return 201 and the created product', async function () {
+    const requestBody = productMock.productBodyCreatePost;
+
+    const response = await chai.request(app)
+      .post('/products')
+      .send(requestBody);
+
+    expect(response.status).to.equal(201);
+  });
 });
