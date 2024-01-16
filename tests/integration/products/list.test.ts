f2574f61c157d566b15286b07a007c91a91dee04
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-16 18:09:35 -0300

    Integretion test GET /products

--- tests/integration/products/list.test.ts
+++ tests/integration/products/list.test.ts
@@ -1,10 +1,22 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
+import ProductModel, { ProductSequelizeModel } from '../../../src/database/models/product.model';
+import productsMock from '../../mocks/products.mock';
+
 
 chai.use(chaiHttp);
 
 describe('GET /products', function () { 
   beforeEach(function () { sinon.restore(); });
+  it('should return 200 OK and a list of Products', async function () {
 
+    sinon.stub(ProductModel, 'findAll')
+      .resolves(productsMock.allProductsFromDB as unknown as ProductSequelizeModel[]);
+
+    const response = await chai.request(app).get('/products');
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.eq(productsMock.allProductsFromDB); 
+  });
 });
