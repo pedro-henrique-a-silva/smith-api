1619fa655952324460e0254bc9987ba9b9a3fd3d
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-16 18:10:10 -0300

    Removing ProductCreateReturntype from Product type file

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,10 +1,6 @@
-import { ServiceResponse } from './ServiceResponse';
-
 export type Product = {
   id: number;
   name: string;
   price: string;
   orderId: number;
 };
-
-export type ProductCreateReturnType = ServiceResponse<Omit<Product, 'orderId'>>;
