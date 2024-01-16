8e6a983f13826f180507667e73307219bf183fbc
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-16 16:35:58 -0300

    Add new route proucts to app, add midleware for json bodys

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
+import routesProducts from './routes/products.routes';
+
 const app = express();
+app.use(express.json());
 
 app.use(express.json());
+app.use('/products', routesProducts);
 
 export default app;
