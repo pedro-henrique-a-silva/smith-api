4894563222770164e004232b5c1b92947ea47f8c
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-16 18:11:37 -0300

    Crete get all products function & type of functions return

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,5 +1,10 @@
-import ProductModel, { ProductInputtableTypes } from '../database/models/product.model';
-import { ProductCreateReturnType } from '../types/Product';
+import ProductModel, { 
+  ProductInputtableTypes, ProductSequelizeModel } from '../database/models/product.model';
+import { Product } from '../types/Product';
+import { ServiceResponse } from '../types/ServiceResponse';
+
+export type ProductCreateReturnType = ServiceResponse<Omit<Product, 'orderId'>>;
+export type ProductAllReturnType = ServiceResponse<ProductSequelizeModel[]>;
 
 const create = async (
   productValues: ProductInputtableTypes,
@@ -15,6 +20,13 @@ const create = async (
   return { status: 'CREATED', data: prouctData };
 };
 
+const all = async (): Promise<ProductAllReturnType> => {
+  const products = await ProductModel.findAll();
+
+  return { status: 'SUCCESS', data: products };
+};
+
 export default {
   create,
+  all,
 };
