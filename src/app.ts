c2b8947fa0bcfbe6a824e3f6f364537b4d882e31
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-17 08:11:18 -0300

    Add login route into app file

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import routesProducts from './routes/products.routes';
 import routesOrder from './routes/orders.routes';
+import routesLogin from './routes/login.routes';
 
 const app = express();
 app.use(express.json());
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use(express.json());
 app.use('/products', routesProducts);
 app.use('/orders', routesOrder);
+app.use('/login', routesLogin);
 
 export default app;
