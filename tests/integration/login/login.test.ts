cb9901908fd3973e9c6a577b021d91a5d9638bf5
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-17 15:24:40 -0300

    Refactoring integration test login

--- tests/integration/login/login.test.ts
+++ tests/integration/login/login.test.ts
@@ -3,6 +3,7 @@ import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../../../src/app';
 import loginMock from '../../mocks/login.mock';
+import LoginModel from '../../../src/database/models/user.model';
 
 chai.use(chaiHttp);
 
@@ -10,7 +11,9 @@ describe('POST /login', function () {
   beforeEach(function () { sinon.restore(); });
   it('should return 200 and key token', async function () {
     const requestBody = loginMock.loginMockBody;
-
+    const mockReturnFromDb = LoginModel.build(loginMock.loginMockFromDB);
+    sinon.stub(LoginModel, 'findOne')
+      .resolves(mockReturnFromDb);
 
     const response = await chai.request(app)
       .post('/login')
