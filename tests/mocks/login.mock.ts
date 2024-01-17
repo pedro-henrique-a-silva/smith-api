99d023dfaa683361a1c378f7f463bcf6f83dfbb1
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-17 15:25:18 -0300

    creating login mocks

--- tests/mocks/login.mock.ts
+++ tests/mocks/login.mock.ts
@@ -3,6 +3,27 @@ const loginMockBody = {
   password: "terrível"
 };
 
+const loginMockBodyWrongPassword = {
+  username: "Hagar",
+  password: "wrongPassword"
+};
+
+const loginMockBodyWithInexistentUser = {
+  username: "InexistentUser",
+  password: "terrível"
+};
+
+const loginMockFromDB = {
+  id: 1,
+  vocation: 'Guerreiro',
+  level: 10,
+  username: "Hagar",
+  password: "$2a$10$Zefi8EQvEjdGE3vyzuDnueSnY3k/nnFJWSHOCntES3yI.YlAmGR8e"
+}
+
 export default {
-  loginMockBody
+  loginMockBody,
+  loginMockFromDB,
+  loginMockBodyWithInexistentUser,
+  loginMockBodyWrongPassword
 }
