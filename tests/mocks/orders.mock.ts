b22abbffedc6e8e3c2a1f36a17664cf879fc3c65
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-17 15:25:33 -0300

    refactoring orders mock

--- tests/mocks/orders.mock.ts
+++ tests/mocks/orders.mock.ts
@@ -1,37 +1,24 @@
-const allOrdersMockFromDB = [
-  {
-    id: 1,
-    userId: 1,
-  },
-  {
-    id: 2,
-    userId: 3,
-  },
-  {
-    id: 3,
-    userId: 2,
-  }
-]
+import OrderModel from "../../src/database/models/order.model"
+
 
 const allOrdersMockBody = [
-  {
+  OrderModel.build({
     id: 1,
     userId: 1,
     productIds: [ 1, 2 ]
-  },
-  {
+  }),
+  OrderModel.build({
     id: 2,
     userId: 3,
     productIds: [ 3, 4 ]
-  },
-  {
+  }),
+  OrderModel.build({
     id: 3,
     userId: 2,
     productIds: [ 5 ]
-  }
+  })
 ]
 
 export default {
-  allOrdersMockFromDB,
   allOrdersMockBody
 }
