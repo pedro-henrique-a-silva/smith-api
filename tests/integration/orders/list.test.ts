fed42bf3d1855705f496705d6311ec28aa39b5de
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-16 22:05:33 -0300

    Integration test GET /orders

--- tests/integration/orders/list.test.ts
+++ tests/integration/orders/list.test.ts
@@ -1,10 +1,27 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import OrderModel, { OrderSequelizeModel } from '../../../src/database/models/order.model';
+import app from '../../../src/app';
+import ordersMock from '../../mocks/orders.mock';
+import productsMock from '../../mocks/products.mock';
+import ProductModel, { ProductSequelizeModel } from '../../../src/database/models/product.model';
 
 chai.use(chaiHttp);
 
 describe('GET /orders', function () { 
   beforeEach(function () { sinon.restore(); });
 
+  it('should return 200 OK and a list of Orders', async function () {
+
+    // sinon.stub(ProductModel, 'findAll')
+    //   .resolves(productsMock.allProductsGroupedFromDB as unknown as ProductSequelizeModel[]);
+
+    // sinon.stub(OrderModel, 'findAll')
+    //   .resolves(ordersMock.allOrdersMockFromDB as unknown as OrderSequelizeModel[]);
+
+    const response = await chai.request(app).get('/orders');
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.eq(ordersMock.allOrdersMockBody); 
+  });
 });
