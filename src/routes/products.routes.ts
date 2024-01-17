69344633a45b118b368dab60fea4dd164064125e
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-17 15:23:59 -0300

    add validaty body midleware to route

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import productsController from '../controllers/products.controller';
+import validProductBody from '../middleware/product.middleware';
 
 const productRouter = express.Router();
 
-productRouter.post('/', productsController.create);
+productRouter.post('/', validProductBody, productsController.create);
 productRouter.get('/', productsController.all);
 
 export default productRouter;
