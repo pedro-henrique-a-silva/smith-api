598d0f7bf29573905274037af22eb1e2c6eeb7c9
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-16 22:04:39 -0300

    Add route /orders into app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import routesProducts from './routes/products.routes';
+import routesOrder from './routes/orders.routes';
 
 const app = express();
 app.use(express.json());
 
 app.use(express.json());
 app.use('/products', routesProducts);
+app.use('/orders', routesOrder);
 
 export default app;
