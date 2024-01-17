2bbd71e36ba1111fba7f7f2ee3fa32e28001574f
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-17 15:26:52 -0300

    orders service test

--- tests/unit/services/orders.service.test.ts
+++ tests/unit/services/orders.service.test.ts
@@ -1,7 +1,22 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import OrderModel from '../../../src/database/models/order.model';
+import ordersMock from '../../mocks/orders.mock';
+import orderService from '../../../src/services/orders.service';
 
 describe('OrdersService', function () {
   beforeEach(function () { sinon.restore(); });
+
+  it('Should return a list of orders', async  function () {
+    // Arrange
+    sinon.stub(OrderModel, 'findAll')
+      .resolves(ordersMock.allOrdersMockBody);
+
+    // Act
+    const serviceResponse = await orderService.all();
+
+    // Assert
+    expect(serviceResponse.status).to.eq('SUCCESS');
+  });
 
 });
