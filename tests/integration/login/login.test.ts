d1593883c8b5faa5f3041109bc8ef8231009a080
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-17 08:11:55 -0300

    Integration test POST /login

--- tests/integration/login/login.test.ts
+++ tests/integration/login/login.test.ts
@@ -1,10 +1,22 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
+import loginMock from '../../mocks/login.mock';
 
 chai.use(chaiHttp);
 
 describe('POST /login', function () { 
   beforeEach(function () { sinon.restore(); });
+  it('should return 200 and key token', async function () {
+    const requestBody = loginMock.loginMockBody;
 
+
+    const response = await chai.request(app)
+      .post('/login')
+      .send(requestBody);
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.have.property('token');
+  });
 });
