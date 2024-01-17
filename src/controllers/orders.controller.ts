6ed49196d8573595e4fe55b065c968128f6c8ad7
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-17 15:22:20 -0300

    controller all orders

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
 import OrderService from '../services/orders.service';
+import mapStatusHTTP from '../utils/mapStatusHttp';
 
 const all = async (req: Request, res: Response) => {
   const allOrders = await OrderService.all();
-  return res.status(200).json(allOrders);
+  const { status, data } = allOrders;
+  return res.status(mapStatusHTTP(status)).json(data);
 };
 
 export default {
