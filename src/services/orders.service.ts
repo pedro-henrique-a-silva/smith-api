491202366d0431240ad9941a9ed721294d00a1a9
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-17 15:24:20 -0300

    Refactoring all orders service

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,28 +1,25 @@
 import { Sequelize } from 'sequelize';
-import OrderModel from '../database/models/order.model';
+import OrderModel, { OrderSequelizeModel } from '../database/models/order.model';
 import ProductModel from '../database/models/product.model';
-import { Order } from '../types/Order';
+import { ServiceResponse } from '../types/ServiceResponse';
 
-const all = async (): Promise<Order[]> => {
-  const products = await ProductModel.findAll({
-    attributes: ['orderId', [Sequelize.fn('JSON_ARRAYAGG', Sequelize.col('id')), 'productIds']],
-    group: ['orderId'],
-  });
-  const allOrders = await OrderModel.findAll();
-
-  const allOrdersReturn = allOrders.map((order) => {
-    const productFound = products
-      .find((product) => (product.dataValues.orderId === order.dataValues.id));
-    if (productFound) {
-      return {
-        ...order.dataValues,
-        productIds: productFound.dataValues.productIds,
-      };
-    }
-    return order.dataValues;
+const all = async (): Promise<ServiceResponse<OrderSequelizeModel[]>> => {
+  const allOrders = await OrderModel.findAll({
+    include: { 
+      model: ProductModel,
+      attributes: [],
+      as: 'productIds', 
+    },
+    attributes: [
+      'id', 
+      'userId',
+      [Sequelize.fn('JSON_ARRAYAGG', Sequelize.col('productIds.id')), 'productIds'],
+    ],
+    raw: true,
+    group: ['Order.id'],
   });
 
-  return allOrdersReturn;
+  return { status: 'SUCCESS', data: allOrders };
 };
 
 export default {
